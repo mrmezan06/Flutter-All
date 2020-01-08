0ba5ffd742b873256f4cdbe785f012b4ada5eff3
mezan <mrmezan0@gmail.com>
2020-01-08 18:03:04 +0600

    Animation Step 4 : Curve Animation and Another way added in comment section

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,52 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: RaisedButton(
+      body: RaisedButton(
           child: Text('GO!'),
           onPressed: (){
             Navigator.of(context).push(_createRoute());
           },
         ),
-      ),
+
     );
   }
 
 
 }
+//Another way to creating transition
+/*
+transitionsBuilder: (context, animation, secondaryAnimation, child) {
+  var begin = Offset(0.0, 1.0);
+  var end = Offset.zero;
+  var curve = Curves.ease;
 
+  var tween = Tween(begin: begin, end: end);
+  var curvedAnimation = CurvedAnimation(
+   parent: animation,
+   curve: curve,
+  );
+
+  return SlideTransition(
+   position: tween.animate(curvedAnimation),
+   child: child,
+  );
+}
+*/
 Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => Page2(),
-    transitionsBuilder: (context, animation , secondaryAnimation, child ){
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
       var begin = Offset(0.0, 1.0);
       var end = Offset.zero;
-      var tween = Tween(begin: begin, end: end);
-      var offsetAnimation = animation.drive(tween);
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
       return SlideTransition(
-        position: offsetAnimation,
+        position: animation.drive(tween),
         child: child,
       );
-    }
+    },
   );
 }
 
