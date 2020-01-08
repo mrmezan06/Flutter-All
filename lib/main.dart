e197163573e3a5a30dcf318fd94890d94a825fdd
mezan <mrmezan0@gmail.com>
2020-01-08 17:36:28 +0600

    BMI Calculator

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'bmipage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown]);
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+      // routes: <String, WidgetBuilder>{
+      //   '/': (context) => HomePage(),
+      //   '/second': (context) => SecondApp()
+      // },
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BmiPage()
+    );
+  }
+}
