3e299686ecb6eefbfb058766076028e3baeb5bb8
mezan <mrmezan0@gmail.com>
2020-01-17 05:50:01 +0600

    Finishing the login screen App

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,26 @@ class LoginPage extends StatefulWidget {
   }
 
 }
-class LoginPageState extends State<LoginPage>{
+class LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
+  
+  AnimationController iconAnimController;
+  Animation<double> iconAnimation;
+  
+  @override
+  void initState() {
+    super.initState();
+    iconAnimController = new AnimationController(
+        vsync:this,
+        duration: new Duration(milliseconds: 500)
+    );
+    iconAnimation = new CurvedAnimation(
+        parent: iconAnimController,
+        curve: Curves.easeOut
+    );
+    iconAnimation.addListener(()=> this.setState((){}));
+    iconAnimController.forward();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -32,7 +51,62 @@ class LoginPageState extends State<LoginPage>{
         children: <Widget>[
           new Image(image: AssetImage('assets/girl.jpg'),
             fit: BoxFit.cover,
-
+             color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: iconAnimation.value * 100,
+              ),
+              new Form(
+                child: Theme(
+                  data: new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.teal,
+                    inputDecorationTheme: InputDecorationTheme(
+                      labelStyle: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0
+                      )
+                    )
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                         new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: 'Enter Email',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: 'Enter Password',
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        new Padding(padding: const EdgeInsets.only(top: 40.0)),
+                        MaterialButton(
+                          height: 40.0,
+                          minWidth: 100.0,
+                          color: Colors.green,
+                          textColor: Colors.white,
+                          child: Icon(Icons.laptop),
+//                        child: Text('Login'),
+                          onPressed: ()=>{},
+                          splashColor: Colors.redAccent,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           )
         ],
       ),
